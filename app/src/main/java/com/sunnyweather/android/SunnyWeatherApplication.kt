37dbc512693a795b857d5d3a5139e590273b26e8
mingshen <ming.shen@sycantechs.com>
2020-06-07 12:14:54 +0800

    实现搜索全球城市数据功能

---
+++ app/src/main/java/com/sunnyweather/android/SunnyWeatherApplication.kt
@@ -0,0 +1,13 @@
+package com.sunnyweather.android
+
+import android.annotation.SuppressLint
+import android.app.Application
+import android.content.Context
+
+class SunnyWeatherApplication:Application() {
+    companion object{
+        const val TOKEN=""
+        @SuppressLint("StaticFieldLeak")
+        lateinit var context: Context
+    }
+}
